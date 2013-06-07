56e7d1b34e11e83b2cf1650b3b090917c94ff5c3
gasolin <gasolin@gmail.com>
2013-06-07 17:01:49 +0800

    homepage house cleaning

--- minilla.py
+++ minilla.py
@@ -4,57 +4,21 @@
 # minilla is a monster named from Ultraman gaia http://en.wikipedia.org/wiki/Minilla
 #
 
-from bottle import route, run, template, request, view
+from bottle import route, run, template, request, view, app
 import preload
 import os, glob, json
 
-HEADER = """
-<!DOCTYPE html>
-<html lang='en'>
-  <head>
-    <meta charset='UTF-8'>
-    <title>Let's work for the Open Web</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <meta name='description' content='Minilla by Mozilla Taiwan'>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="dns-prefetch" href="//ajax.googleapis.com">
-    <link rel="dns-prefetch" href="//netdna.bootstrapcdn.com">
-
-    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'></script>
-    <link href='http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css' rel='stylesheet'>
-    <!--script src='http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js'></script-->
-    <link href='http://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.min.css' rel='stylesheet'>
-    <style>
-    html, body {
-	  margin: 0;
-	  padding: 0;
-	}
-    </style>
-  </head>
-  <body>
-    <div class="navbar navbar-inverse">
-      <div class="navbar-inner">
-      <a class="brand" href="/">Minilla</a>
-      <ul class="nav">
-      <li><a href="/homescreen">homescreen</a></li>
-      <li><a href="/icon">icon convertor</a></li>
-      </ul>
-      </div>
-    </div>
-    <div class="container-fluid">
-"""
-FOOTER = """
-    </div>
-  </body>
-</html>
-"""
+# application object required by wsgi, appfog use gunicorn with wsgi
+application = app()
+# setup environment
+DEFAULT_EXTERNAL_APP_PATH = os.path.join("gaia-raw", "external-apps")
+if not os.path.exists(DEFAULT_EXTERNAL_APP_PATH):
+    os.makedirs(DEFAULT_EXTERNAL_APP_PATH)
 
 @route('/')
+@view('index')
 def index():
-  page = HEADER + """
-  welcome
-  """ + FOOTER
-  return template(page)
+  return dict()
 
 # icon handler
 @route('/icon')
@@ -137,11 +101,9 @@ def webapp():
 @route('/apps/', method='post')
 def apps():
     app_url = request.forms.get('app_url')
-    manifest = preload.fetch_webapp(app_url, os.path.join("gaia-raw", "external-apps"))
+    manifest = preload.fetch_webapp(app_url, DEFAULT_EXTERNAL_APP_PATH)
     return {'name': manifest['name']}
 
 # run server
 if __name__ == '__main__':
-    import bottle
-    app = bottle.app()
-    run(app, host='localhost', port=8000, reloader=True)
+    run(application, host='localhost', port=8000, reloader=True)
